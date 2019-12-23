5f0c5760ba257b405539e0b6e36e0d19902bd1aa
m4k5 <m4k5@centro.team>
2019-12-24 00:09:44 +0200

    add consts in init.php

---
+++ config/init.php
@@ -0,0 +1,19 @@
+<?php
+
+define("DEBUG", 1);
+define("ROOT", dirname(__DIR__));
+define("WWW", ROOT . '/public');
+define("APP", ROOT . '/app');
+define("CORE", ROOT . '/vendor/shop/core');
+define("LIBS", ROOT . '/vendor/shop/core/libs');
+define("CACHE", ROOT . '/tmp/cache');
+define("CONF", ROOT . '/config');
+define("LAYOUT", 'default');
+
+$app_path = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
+define("PATH", $app_path);
+define("ADMIN", PATH . '/admin');
+//$app_path = preg_replace("#[^/]+$#", '', $app_path);
+
+require_once ROOT . '/vendor/autoload.php';
